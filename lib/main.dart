b36fe40e681a97170d406f2d1f0c087f56acc994
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-01-24 15:00:33 +0600

    about list tile  w

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widgets/about_dialog_widget.dart';
+import 'package:flutter_widgets/about_list_tile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const Scaffold(
-        body: AboutDialogWidget(),
+        body: AboutListTileW(),
       ),
     );
   }
