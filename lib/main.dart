2d75815f5f792a44aaad9740b9e4ac1681acffe2
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-01-25 11:29:43 +0600

    absorb pointer w

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widgets/about_list_tile.dart';
+import 'package:flutter_widgets/absorb_pointer_widget003.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const Scaffold(
-        body: AboutListTileW(),
+        body: AbsorbPointerWidget003(),
       ),
     );
   }
