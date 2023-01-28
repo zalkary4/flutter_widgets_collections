06318d37420b1ed61fd7dd03001e8171a23c1eb2
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-01-28 08:15:11 +0600

    animated container w

--- lib/animated_container_w008.dart
+++ lib/animated_container_w008.dart
@@ -20,7 +20,7 @@ class _AnimatedContainerW008State extends State<AnimatedContainerW008> {
       child: Center(
           child: AnimatedContainer(
         width: selected ? 300.0 : 200.0,
-        height: selected ? 100.0 : 200.0,
+        height: selected ? 300.0 : 400.0,
         color: selected ? Colors.blueGrey : Colors.white,
         alignment: selected ? Alignment.center : AlignmentDirectional.topCenter,
         duration: const Duration(seconds: 2),
