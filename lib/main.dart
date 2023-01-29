9114124a169dc403e43f9d3634b315401f36413f
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-01-29 11:00:31 +0600

    animated cross fade w

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widgets/animated_container_w009.dart';
+
+import 'widgets/animated_cross_fade_w009.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const Scaffold(
-        body: AnimatedContainerW009(),
+        body: AnimatedCrossFadeW010(),
       ),
     );
   }
---
+++ lib/widgets/animated_cross_fade_w009.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnimatedCrossFadeW010 extends StatefulWidget {
+  const AnimatedCrossFadeW010({Key? key}) : super(key: key);
+
+  @override
+  _AnimatedCrossFadeW010State createState() => _AnimatedCrossFadeW010State();
+}
+
+class _AnimatedCrossFadeW010State extends State<AnimatedCrossFadeW010> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
