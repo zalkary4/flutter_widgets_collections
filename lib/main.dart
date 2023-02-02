d3ba260a81c5dc152e9edd181c9d7b1bdd5a0e35
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-02-02 15:10:52 +0600

    physical shape  w

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'widgets/physical_model_widget.dart';
+import 'widgets/physical_shape_widget2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const Scaffold(
-        body: PhysicalModelWidget(),
+        body: PhysicalShapeWidget2(),
       ),
     );
   }
