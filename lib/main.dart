fa492229fd9f143bb9b34125147f2928114ab2f4
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-02-05 10:25:06 +0600

    positioned stack w

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'widgets/pop_up_menu_widget.dart';
+import 'package:flutter_widgets/positioned_stack_widget02.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const Scaffold(
-        body: PopUpMenuWidget01(),
+        body: PositionedStackWidget02(),
       ),
     );
   }
