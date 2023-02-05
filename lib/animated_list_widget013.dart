19e6bf6e77f0bbff7cbf9f4607206f06e70a0aeb
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-02-05 12:59:03 +0600

    animated list w

--- lib/animated_list_widget013.dart
+++ lib/animated_list_widget013.dart
@@ -1,1 +1,47 @@
+import 'package:flutter/material.dart';
 
+class AnimatedListWidget013 extends StatefulWidget {
+  AnimatedListWidget013({Key? key}) : super(key: key);
+
+  @override
+  _AnimatedListWidget013State createState() => _AnimatedListWidget013State();
+  final _items = [];
+  final GlobalKey<AnimatedListState> _key = GlobalKey();
+  void _addItem() {
+    _items.insert(0, 'Item ${_items.length + 1}');
+    _key.currentState!.insertItem(
+      0,
+      duration: const Duration(seconds: 1),
+    );
+  }
+
+  void _removeItem(int index) {
+    _key.currentState!.removeItem(
+      index,
+      (_, animation) {
+        return SizeTransition(
+          sizeFactor: animation,
+          child: const Card(
+            margin: EdgeInsets.all(10),
+            color: Colors.red,
+            child: ListTile(
+              title: Text(
+                'Delete',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+          ),
+        );
+      },
+      duration: const Duration(microseconds: 300),
+    );
+    _items.removeAt(index);
+  }
+}
+
+class _AnimatedListWidget013State extends State<AnimatedListWidget013> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widgets/widgets/animated_icon_widget012.dart';
+import 'package:flutter_widgets/animated_list_widget013.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,8 +14,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(
-        body: AnimatedIconWidget012(),
+      home: Scaffold(
+        body: AnimatedListWidget013(),
       ),
     );
   }
