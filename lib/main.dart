19a28a98c278d1ed5e3c33c6adb39aeeb1d67313
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-02-05 09:18:30 +0600

    pop up button w

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'widgets/place_holder_widget1.dart';
+import 'widgets/pop_up_menu_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const Scaffold(
-        body: PlaceHolderWidget1(),
+        body: PopUpMenuWidget01(),
       ),
     );
   }
