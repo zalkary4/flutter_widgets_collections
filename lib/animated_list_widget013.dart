dcccf32602b9780a3e2046317dbb05da2a87ada1
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-02-07 17:05:46 +0600

    animated list w

--- lib/animated_list_widget013.dart
+++ lib/animated_list_widget013.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
 class AnimatedListWidget013 extends StatefulWidget {
-  AnimatedListWidget013({Key? key}) : super(key: key);
+  const AnimatedListWidget013({Key? key}) : super(key: key);
 
   @override
   _AnimatedListWidget013State createState() => _AnimatedListWidget013State();
+}
+
+class _AnimatedListWidget013State extends State<AnimatedListWidget013> {
   final _items = [];
   final GlobalKey<AnimatedListState> _key = GlobalKey();
+
   void _addItem() {
-    _items.insert(0, 'Item ${_items.length + 1}');
+    _items.insert(0, "Item ${_items.length + 1}");
     _key.currentState!.insertItem(
       0,
       duration: const Duration(seconds: 1),
@@ -37,11 +41,47 @@ class AnimatedListWidget013 extends StatefulWidget {
     );
     _items.removeAt(index);
   }
-}
 
-class _AnimatedListWidget013State extends State<AnimatedListWidget013> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        const SizedBox(height: 10),
+        IconButton(
+          onPressed: _addItem,
+          icon: const Icon(Icons.add),
+        ),
+        Expanded(
+          child: AnimatedList(
+            key: _key,
+            initialItemCount: 0,
+            padding: const EdgeInsets.all(10),
+            itemBuilder: (context, index, animation) {
+              return SizeTransition(
+                sizeFactor: animation,
+                key: UniqueKey(),
+                child: Card(
+                  margin: const EdgeInsets.all(10),
+                  color: Colors.orangeAccent,
+                  child: ListTile(
+                    title: Text(
+                      _items[index],
+                      style: const TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                    trailing: IconButton(
+                        onPressed: () {
+                          _removeItem(index);
+                        },
+                        icon: const Icon(Icons.delete)),
+                  ),
+                ),
+              );
+            },
+          ),
+        )
+      ],
+    );
   }
 }
