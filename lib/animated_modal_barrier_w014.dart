02654978d0c172a15a016bb5343bc8acf6d10473
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-02-08 14:35:49 +0600

    animated modal barrier w

--- lib/animated_modal_barrier_w014.dart
+++ lib/animated_modal_barrier_w014.dart
@@ -11,19 +11,68 @@ class AnimatedModalBarrierW014 extends StatefulWidget {
 
 class _AnimatedModalBarrierW014State extends State<AnimatedModalBarrierW014>
     with SingleTickerProviderStateMixin {
-  final bool _isPressed = false;
+  bool _isPressed = false;
   late Widget _animatedModalBarrier;
   late AnimationController _animationConntroller;
   late Animation<Color?> _colorAnimation;
   @override
   void initState() {
-    super.initState(
-        // ColorTween _colorTween ();
-        );
+    ColorTween colorTween = ColorTween(
+      begin: Colors.orangeAccent.withOpacity(0.5),
+      end: Colors.blueGrey.withOpacity(0.5),
+    );
+    _animationConntroller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    _colorAnimation = colorTween.animate(_animationConntroller);
+
+    _animatedModalBarrier = AnimatedModalBarrier(
+      color: _colorAnimation,
+      dismissible: true,
+    );
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 100,
+              width: 250,
+              child: Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orangeAccent,
+                    ),
+                    child: const Text('Press'),
+                    onPressed: () {
+                      setState(() {
+                        _isPressed = true;
+                      });
+                      _animationConntroller.reset();
+                      _animationConntroller.forward();
+                      Future.delayed(const Duration(seconds: 1), () {
+                        setState(() {
+                          _isPressed = false;
+                        });
+                      });
+                    },
+                  ),
+                  if (_isPressed) _animatedModalBarrier,
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
