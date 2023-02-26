668a392b9a84af432c0509bc46bea3d31461affc
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-02-26 10:46:00 +0600

    scale transition w

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets/scale_transition_widget.dart';
-import 'package:flutter_widgets/widgets/app_bar_widget026.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,25 +15,12 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: appBarForAllWidgets(),
-        body: const ScaleTransitionWidget(),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Profile',
-            ),
-          ],
-          onTap: (int index) {},
-          selectedItemColor: Colors.orangeAccent,
-        ),
+        // appBar: appBarForAllWidgets(),
+        // bottomNavigationBar: bottomNavBarForAllWidgets(),
+        body: ScaleTransitionWidget(),
       ),
     );
   }
