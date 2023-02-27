c7fa15d99eebe83396e92a428db9822b76be09f5
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-02-27 09:33:28 +0600

    aspect ratio w

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'some_neumorphism_page.dart';
+import 'package:flutter_widgets/aspect_ratio_widget027.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +20,7 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         // appBar: appBarForAllWidgets(),
         // bottomNavigationBar: bottomNavBarForAllWidgets(),
-        body: SomeNeumorphismPage(),
+        body: AspectRatioWidget027(),
       ),
     );
   }
