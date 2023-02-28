42dd3121e048f51235257c46837a1fe7bf78c568
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-02-28 19:46:32 +0600

    scroll bar w

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widgets/selectable_text_widget.dart';
+import 'package:flutter_widgets/scroll_bar_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +20,7 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         // appBar: appBarForAllWidgets(),
         // bottomNavigationBar: bottomNavBarForAllWidgets(),
-        body: SelectableTextWidget(),
+        body: ScrollBarWidget11(),
       ),
     );
   }
