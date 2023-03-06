9f82c7680bf142e8b1201f70b6824d1c97769a6e
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-03-06 22:32:17 +0600

    shortcut w

--- lib/short_cuts_widget.dart
+++ lib/short_cuts_widget.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
+
+class IncrementIntent extends Intent {
+  const IncrementIntent();
+}
+
+class DecrementIntent extends Intent {
+  const DecrementIntent();
+}
 
 class ShortCutsWidget extends StatefulWidget {
   const ShortCutsWidget({Key? key}) : super(key: key);
@@ -8,8 +18,38 @@ class ShortCutsWidget extends StatefulWidget {
 }
 
 class _ShortCutsWidgetState extends State<ShortCutsWidget> {
+  int count = 0;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Shortcuts(
+      shortcuts: <ShortcutActivator, Intent>{
+        LogicalKeySet(LogicalKeyboardKey.arrowUp): const IncrementIntent(),
+        LogicalKeySet(LogicalKeyboardKey.arrowDown): const DecrementIntent(),
+      },
+      child: Actions(
+        actions: {
+          IncrementIntent: CallbackAction(
+            onInvoke: (intent) {
+              setState(
+                () {
+                  count = count + 1;
+                },
+              );
+              return null;
+            },
+          ),
+          DecrementIntent: CallbackAction(
+            onInvoke: (intent) {
+              setState(
+                () {
+                  count = count - 1;
+                },
+              );
+              return null;
+            },
+          )
+        },
+      ),
+    );
   }
 }
