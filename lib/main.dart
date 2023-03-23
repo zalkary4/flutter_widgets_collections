ba7277d09ed3d2e114a1271d312eba111d76f21f
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-03-23 17:22:08 +0600

    sliver padding w

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'sliver_opacity_widget.dart';
+import 'package:flutter_widgets/sliver_paddin_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +20,7 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         // appBar: appBarForAllWidgets(),
         // bottomNavigationBar: bottomNavBarForAllWidgets(),
-        body: SliverOpacityWidget(),
+        body: SliverPaddinWidget(),
       ),
     );
   }
